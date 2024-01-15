d1860c56e3a88306071826f2b6efa5037c38288e
mariamabah21 <mbah3263@gmail.com>
2024-01-15 23:36:33 +0100

    last changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import { ThemeProvider } from "styled-components";
 import { SkeletonTheme } from "react-loading-skeleton";
+import { ToastContainer } from "react-toastify";
 import { theme } from "styles/Theme";
 import Home from "pages/Home";
 import { GlobalStyles } from "styles/Global";
 import Header from "components/Header";
-
 // Import skeleton loader css
 import "react-loading-skeleton/dist/skeleton.css";
+
+// Import react tostify css
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
@@ -18,11 +21,22 @@ function App() {
         <GlobalStyles />
         <Header />
         <Home />
+        <ToastContainer
+          position="bottom-left"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="dark"
+        />
       </SkeletonTheme>
     </ThemeProvider>
   );
 }
-
 export default App;
 
 // ``;
